9fe4276c1911022a2d4e2b56ef4768bc5fd3d6bd
EinePerson <einenperson@gmail.com>
2024-03-07 17:52:46 +0100

    Initial commit

---
+++ src/main/java/de/leafstudios/Listener.java
@@ -0,0 +1,14 @@
+package de.leafstudios;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.GenericMessageEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class Listener extends ListenerAdapter {
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        event.reply("You have gone gaming").queue();
+    }
+
+}
